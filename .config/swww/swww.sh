662e8f0fcf97a7c51d643e9a4375125cce447f83
IU-IANG <iuiangiulg@gmail.com>
2023-07-26 12:33:35 +0800

    swww

---
+++ .config/swww/swww.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR=$HOME/.config/wallpaper/
+
+
+PICS=($(ls ${DIR}))
+
+RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
+
+if [[ $(pidof swaybg) ]]; then
+  pkill swaybg
+fi
+
+swww query || swww init
+
+swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
