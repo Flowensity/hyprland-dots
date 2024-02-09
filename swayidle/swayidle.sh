4f8669ba92847e50a6e4f9d7df4942bfd821f646
Flowensity <flowensity@proton.me>
2024-02-09 20:19:20 +0800

    fix some bug

--- swayidle/swayidle.sh
+++ swayidle/swayidle.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-swayidle -w \
-	timeout 900 'swaylock -f -c 000000' \
-	timeout 1200 'hyprctl dispatch dpms off && systemctl suspend' \
-	resume 'hyprctl dispatch dpms on' \
-	before-sleep 'swaylock -f -c 000000'
+swayidle -w timeout 600 'swaylock -f' \
+            timeout 900 'hyprctl dispatch dpms off' \
+            resume 'hyprctl dispatch dpms on' \
+            before-sleep 'swaylock -f' &
