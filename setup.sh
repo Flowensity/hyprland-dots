dab4a0987f63281d53bd85d492caf1599a1ff297
Flowensity <flowensity@proton.me>
2024-02-10 00:14:25 +0800

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-hyprland='$HOME/hyprland-dots'
+hyprland="$HOME/hyprland-dots" 
 
 # Installing paru
 echo "Installing paru"
@@ -8,32 +8,34 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-# Installing Dependencies(arch only)
+# Installing Dependencies (arch only)
 echo "Installing Dependencies"
-prau -S kitty hyprland waybar dust nwg-bar nwg-look swaylock-effects catppuccin-gtk-theme-macchiato \
+paru -S kitty hyprland waybar dust nwg-bar nwg-look swaylock-effects catppuccino-gtk-theme-macchiato \
   tela-icon-theme-bin cava neofetch qt5ct nwg-look swww wofi swayidle grim slurp qt5-wayland qt6-wayland \
-  polkit-kde-agent xdg-desktop-portal cliphist udiskie sddm-git zsh
+  polkit-kde-agent xdg-desktop-portal clipman udiskie sddm-git zsh 
 
-
-# Installing Apps(arch only)
+# Installing Apps (arch only)
 echo "Installing Apps"
 paru -S librewolf-bin brave-bin thunar waydroid mpv koodo-reader-bin kdeconnect linux-wifi-hotspot \
   obs-studio motrix-bin rustdesk-bin
 
-ln -s $HOME/hyprland/kitty ~/.config
-ln -s $HOME/hyprland/nwg-look ~/.config
-ln -s $HOME/hyprland/scripts ~/.config
-ln -s $HOME/hyprland/swaync ~/.config
-ln -s $HOME/hyprland/wofi ~/.config
-ln -s $HOME/hyprland/cava ~/.config
-ln -s $HOME/hyprland/neofetch ~/.config
-ln -s $HOME/hyprland/wallpoaper ~/.config
-ln -s $HOME/hyprland/xsettingsd ~/.config
-ln -s $HOME/hyprland/hypr ~/.config
-ln -s $HOME/hyprland/nwg-bar ~/.config
-ln -s $HOME/hyprland/qt6ct ~/.config
-ln -s $HOME/hyprland/swaylock ~/.config
-ln -s $HOME/hyprland/waybar ~/.config
-ln -s $HOME/hyprland/setup.sh ~/setup.sh
-ln -s $HOME/hyprland/.zshrc ~/.zshrc
-ln -s $HOME/hyprland/.oh-my-zsh ~/.oh-my-zsh
+# 创建软链接
+echo "Creating symbolic links"
+ln -s $hyprland/kitty ~/.config
+ln -s $hyprland/nwg-look ~/.config
+ln -s $hyprland/scripts ~/.config
+ln -s $hyprland/swaync ~/.config
+ln -s $hyprland/wofi ~/.config
+ln -s $hyprland/cava ~/.config
+ln -s $hyprland/neofetch ~/.config
+ln -s $hyprland/wallpaper ~/.config 
+ln -s $hyprland/xsettingsd ~/.config
+ln -s $hyprland/hypr ~/.config
+ln -s $hyprland/nwg-bar ~/.config
+ln -s $hyprland/qt6ct ~/.config
+ln -s $hyprland/swaylock ~/.config
+ln -s $hyprland/waybar ~/.config
+ln -s $hyprland/setup.sh ~/setup.sh
+ln -s $hyprland/.zshrc ~/.zshrc
+ln -s $hyprland/.oh-my-zsh ~/.oh-my-zsh
+
