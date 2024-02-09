bd0b1bb74f24ccc9a5ae6e97591ef2dc51bdbeb7
Flowensity <flowensity@proton.me>
2024-02-09 23:35:00 +0800

    add .oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 hyprland='$HOME/hyprland-dots'
 
+# Downlaod dots
+echo "Downloading Dotfiles"
+cd && https://github.com/Flowensity/hyprland-dots.git
+
 # Installing Dependencies(arch only)
 echo "Installing Dependencies"
 prau -S kitty hyprland waybar dust nwg-bar nwg-look swaylock-effects catppuccin-gtk-theme-macchiato \
   tela-icon-theme-bin cava neofetch qt5ct nwg-look swww wofi swayidle grim slurp qt5-wayland qt6-wayland \
-  polkit-kde-agent xdg-desktop-portal cliphist udiskie sddm-git
+  polkit-kde-agent xdg-desktop-portal cliphist udiskie sddm-git zsh
 
 
 # Installing Apps(arch only)
 echo "Installing Apps"
 paru -S librewolf-bin brave-bin thunar waydroid mpv koodo-reader-bin kdeconnect linux-wifi-hotspot \
   obs-studio motrix-bin rustdesk-bin
-
-# Downlaod dots
-echo "Downloading Dotfiles"
-cd && https://github.com/Flowensity/hyprland-dots.git
 
 ln -s $HOME/hyprland/kitty ~/.config
 ln -s $HOME/hyprland/nwg-look ~/.config
@@ -33,4 +33,3 @@ ln -s $HOME/hyprland/swaylock ~/.config
 ln -s $HOME/hyprland/waybar ~/.config
 ln -s $HOME/hyprland/setup.sh ~/setup.sh
 ln -s $HOME/.zshrc ~/.zshrc
-
