94b698878b70456fc598e8234083212b398d9375
Flowensity <flowensity@proton.me>
2024-02-12 22:49:35 +0800

    add setup.sh

--- setup.sh
+++ setup.sh
@@ -26,21 +26,21 @@ paru -S librewolf-bin brave-bin thunar waydroid mpv koodo-reader-bin kdeconnect 
 
 # Creating symbolic links
 echo "Creating symbolic links"
-ln -s $hyprland/kitty ~/.config
-ln -s $hyprland/nwg-look ~/.config
-ln -s $hyprland/scripts ~/.config
-ln -s $hyprland/swaync ~/.config
-ln -s $hyprland/wofi ~/.config
-ln -s $hyprland/cava ~/.config
-ln -s $hyprland/neofetch ~/.config
-ln -s $hyprland/wallpaper ~/.config 
-ln -s $hyprland/xsettingsd ~/.config
-ln -s $hyprland/hypr ~/.config
-ln -s $hyprland/nwg-bar ~/.config
-ln -s $hyprland/qt6ct ~/.config
-ln -s $hyprland/swaylock ~/.config
-ln -s $hyprland/waybar ~/.config
+ln -s $hyprland/kitty/* ~/.config
+ln -s $hyprland/nwg-look/* ~/.config
+ln -s $hyprland/scripts/* ~/.config
+ln -s $hyprland/swaync/* ~/.config
+ln -s $hyprland/wofi/* ~/.config
+ln -s $hyprland/cava/* ~/.config
+ln -s $hyprland/neofetch/* ~/.config
+ln -s $hyprland/wallpaper/* ~/.config 
+ln -s $hyprland/xsettingsd/* ~/.config
+ln -s $hyprland/hypr/* ~/.config
+ln -s $hyprland/nwg-bar/* ~/.config
+ln -s $hyprland/qt6ct/* ~/.config
+ln -s $hyprland/swaylock/* ~/.config
+ln -s $hyprland/waybar/* ~/.config
 ln -s $hyprland/setup.sh ~/setup.sh
 ln -s $hyprland/.zshrc ~/.zshrc
-ln -s $hyprland/.oh-my-zsh ~/.oh-my-zsh
+ln -s $hyprland/.oh-my-zsh/* ~/.oh-my-zsh
 
